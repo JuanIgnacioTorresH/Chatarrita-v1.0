e823aad8cfbe329a599ed371261390fe20e88f62
JuanIgnacioTorresH <81965096+JuanIgnacioTorresH@users.noreply.github.com>
2023-06-30 01:02:59 -0300

    Update Nacho - Obstaculo.py

--- Nacho - Obstaculo.py
+++ Nacho - Obstaculo.py
@@ -1,22 +1,23 @@
-from machine import Pin
+from machine import Pin, ADC
 import time
 #Importo las funciones para no tener que escribir tanto
 
-trigger = Pin(27, Pin.OUT)
-echo = Pin (26, Pin.IN)
-infr = Pin (7, Pin.IN)
+trigger = Pin(20, Pin.OUT)
+echo = Pin (21, Pin.IN)
+infr = ADC(Pin(26))
+ivalue1 = 0
 #Asigno pines del ultrasonido y del infrarojo
 
-In3 = Pin (13, Pin.OUT)
-In3 = Pin (12, Pin.OUT)
+In1 = Pin (13, Pin.OUT)
+In2 = Pin (12, Pin.OUT)
 In3 = Pin (11, Pin.OUT)
-In3 = Pin (10, Pin.OUT)
-#Asigno pines de las ruedas
+In4 = Pin (10, Pin.OUT)
+#Checkea que sean esos los pines para cada uno, asigne a cualquiera
 
 EN_A = Pin (14, Pin.OUT)
 EN_B = Pin (9, Pin OUT)
 EN_A.high()
-EN_B_high()
+EN_B.high()
 #Habilitadores
 
 def ultra ():
@@ -41,6 +42,12 @@ def move_forward():
     In4.low()
     #Funcion para ir adelante
 
+def move_backwards():
+    In1.low()
+    In2.high()
+    In3.low()
+    In4.high()
+
 def turn_right():
     In1.low()
     In2.low()
@@ -58,19 +65,33 @@ def stop():
 while True:
     ultra()
     #Uso la funcion de ultrasonido para ver si tengo cajas en frente
+    ivalue = ((infr.read_u16())*3) / 65336
     if distancia < 150:
-        #Asumo que la distancia maxima para que hayan obstaculos es 150
-        if infr.value() == 0:
-            while infr.value() == 0:
+        #150 siendo el tamano total de la pista
+        if infr.value() >= 3:
+            #Necesito la tension umbral para distinguir entre blanco y negro, usando 3 como ejemplo
+            while infr.value() == 3:
                 move_forward()
                 #Si detecta algo y el infrarojo detecta que todavia no va a estar fuera del cuadrante, el robot avanza
-        else:
-            stop()
-            #Si detecta que hay algo, pero esta por irse del cuadrante, da prioridad a detenerse
+    
+        stop()
+        sleep_us(2)
+        time1 = utime_ticks_s()
+        while utime_ticks_diff(utime_ticks_s - time1) < 1:
+            move_backwards()
+        sleep_us(2)
+        stop()
+        sleep_us(2)
+        time1 = utime_ticks_s()
+        while utime_ticks_diff(utime_ticks_s - time1) < 5:
+            turn_right() 
+        sleep_us(2)
+        #Si detecta que hay un objeto pero este en zona negra, o ya estaba llevando un objeto y alcanzo el borde, se detiene, va para atras un segundo y deberia dar un giro de 180(dar variable) 
     else:
         time1 = utime_ticks_s()
         while utime_ticks_diff(utime_ticks_s - time1) < 1:
             turn_right()
+        sleep_us(2)
         stop()
         sleep_us (2)
         #Si no detecta nada, va a girar a la derecha por 1 segundo, detenerse y comenzar el ciclo de nuevo
